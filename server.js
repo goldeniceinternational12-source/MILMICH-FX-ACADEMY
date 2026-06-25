1a68435b9059c4741daf8219d6faa7e89283322c
Princefx973 <princebelonwu@gmail.com>
2026-06-25 12:48:30 +0100

    Updated website content

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
-require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const connectDB = require("./config/db");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://goldeniceinternational12_db_user:Somtochukwu12@cluster0.bvq4po6.mongodb.net/milmichfxacademy?retryWrites=true&w=majority&appName=Cluster0";
 
-const app = express();
-
-connectDB();
-
-app.use(cors());
-app.use(express.json());
-
-app.use(express.static(__dirname));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-app.get("/test", (req, res) => {
-  res.send("MILMICH TEST ROUTE");
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
